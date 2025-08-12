cfda842ff0e1a6142a6053b283b4ef06fc501cc0
john <john_swk17@163.com>
2025-08-12 09:52:21 +0800

    第一次 前端页面

---
+++ src/main/resources/static/data/baseData.js
@@ -0,0 +1,34 @@
+var selectData=[
+    {"weight":0,"text":"无"},
+    {"weight":0.07,"text":"塑料轴"},
+    {"weight":0.1,"text":"8228纸轴"},
+    {"weight":0.15,"text":"7075 / 7070"},
+    {"weight":0.15,"text":"70D24F/本白"},
+    {"weight":0.21,"text":"70D48F本白 150D"},
+    {"weight":0.21,"text":"大轴子尼龙"},
+    {"weight":0.19,"text":"灰氨纶"},
+    {"weight":0.21,"text":"客供大轴"},
+    {"weight":0.14,"text":"客供小轴"},
+    {"weight":0.06,"text":"玲珑轴"},
+    {"weight":0.1,"text":"青岛氨纶轴"},
+    {"weight":0.05,"text":"纱"},
+    {"weight":0.09,"text":"鑫冉"}
+
+];
+
+var personData=[
+    {"personCode":"0090","name":"谷开荣"},
+    {"personCode":"0086","name":"周乃红"},
+    {"personCode":"0810","name":"蒋爱萍"},
+    {"personCode":"0091","name":"朱红霞"},
+    {"personCode":"0092","name":"胡芳"},
+    {"personCode":"0827","name":"石春霞"},
+    {"personCode":"0956","name":"赵艳霞"}, 
+    {"personCode":"1107","name":"左思琳"},
+
+    
+    {"personCode":"0089","name":"朱美玲"},
+    {"personCode":"0995","name":"潘菊华"},
+    {"personCode":"1147","name":"黄颖"},
+    {"personCode":"0722","name":"沈娟娟"}
+];
